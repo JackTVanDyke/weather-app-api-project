ef4ca9a8ce591f1cef435469d898143b37545318
Jack VanDyke <j.vandyket@gmail.com>
2021-09-17 14:35:52 -0400

    http to https

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ form.addEventListener("submit", async function (e) {
   const apiKey = "00g6jLhrd8sme7PiaeXS3F9ztyvKmPk9";
   const searchInput = form.elements.query.value;
   const locationSearch = await axios.get(
-    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${searchInput}`
+    `https://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${searchInput}`
   );
   const locationKey = locationSearch.data[0].Key;
   const weatherResult = await axios.get(
-    `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
+    `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
   );
   city.textContent = `${locationSearch.data[0].EnglishName}, ${locationSearch.data[0].AdministrativeArea.EnglishName}`;
   temp.textContent = `${weatherResult.data[0].Temperature.Imperial.Value} °F`;
