9c8d6ae4270f1e8fa16156964442a9f3721eb531
Jack VanDyke <j.vandyket@gmail.com>
2021-09-17 14:17:57 -0400

    it is finished

---
+++ readme.md
@@ -0,0 +1,1 @@
+WOOOOO! Got some practice with API's and got one to work! Still need to figure out HTTPS and CORS requirements and how they relate to my live-server extension. Until then, this is still a celebration! Seriously, use it. Please.
--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-const api = {
-  key: "e51d5e4fdf7d261ea08cb16858874324",
-  baseurl: "api.openweathermap.org/data/2.5/",
-};
+const form = document.querySelector("#search-form");
+const city = document.querySelector(".city");
+const date = document.querySelector(".date");
+const temp = document.querySelector(".temp");
+const weather = document.querySelector(".weather");
 
-const searchBox = document.querySelector(".search-box");
-searchBox.addEventListener("keypress", setQuery);
-
-function setQuery(e) {
-  if (e.keyCode == 13) {
-    getResults(searchBox.value);
-    console.log(searchBox.value);
-  }
+//live clock bro!
+function time() {
+  var today = new Date();
+  var calendarDate =
+    today.getMonth() + 1 + "-" + today.getDate() + "-" + today.getFullYear();
+  var time =
+    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  var dateTime = calendarDate + " @ " + time;
+  date.textContent = `${dateTime}`;
 }
+setInterval(time, 1000);
 
-function getResults(query) {
-  fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-    .then((weather) => {
-      return weather.json();
-    })
-    .then(displayResults);
-}
-
-function displayResults(weather) {
-  console.log(weather);
-}
+//search function and update content!
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const apiKey = "00g6jLhrd8sme7PiaeXS3F9ztyvKmPk9";
+  const searchInput = form.elements.query.value;
+  const locationSearch = await axios.get(
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${searchInput}`
+  );
+  const locationKey = locationSearch.data[0].Key;
+  const weatherResult = await axios.get(
+    `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
+  );
+  city.textContent = `${locationSearch.data[0].EnglishName}, ${locationSearch.data[0].AdministrativeArea.EnglishName}`;
+  temp.textContent = `${weatherResult.data[0].Temperature.Imperial.Value} °F`;
+  weather.textContent = `${weatherResult.data[0].WeatherText}`;
+});
